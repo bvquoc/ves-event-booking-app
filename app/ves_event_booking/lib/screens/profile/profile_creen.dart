dc7a82b5a979a0e4b5fa641a4bc3173a0bb63f34
HiamSonn <dangthanhson2016@gmail.com>
2025-12-24 16:44:05 +0700

    fix profile UI

--- app/ves_event_booking/lib/screens/profile/profile_creen.dart
+++ app/ves_event_booking/lib/screens/profile/profile_creen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:ves_event_booking/models/user/user_model.dart';
 import 'package:ves_event_booking/providers/user_provider.dart';
 import 'package:ves_event_booking/screens/explore/explore_screen.dart';
 import 'package:ves_event_booking/screens/favourite_events_screen.dart';
@@ -18,6 +19,16 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class ProfileScreenState extends State<ProfileScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (!mounted) return;
+      context.read<UserProvider>().fetchUserInfo();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<UserProvider>(
@@ -46,7 +57,7 @@ class ProfileScreenState extends State<ProfileScreen> {
             children: [
               // === PHẦN CỐ ĐỊNH: HEADER + INFO ===
               _buildScrollableMenu(context, provider),
-              _buildHeader(context),
+              _buildHeader(context, provider.user!),
 
               // === PHẦN CUỘN: CHỈ MENU ===
               // Expanded(
@@ -60,7 +71,7 @@ class ProfileScreenState extends State<ProfileScreen> {
     );
   }
 
-  Widget _buildHeader(BuildContext context) {
+  Widget _buildHeader(BuildContext context, UserModel user) {
     return Stack(
       clipBehavior: Clip.none,
       children: [
@@ -80,10 +91,10 @@ class ProfileScreenState extends State<ProfileScreen> {
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Row(
               children: [
-                const Expanded(
+                Expanded(
                   child: Text(
                     'Trang cá nhân',
-                    style: TextStyle(
+                    style: const TextStyle(
                       color: Colors.black,
                       fontSize: 18,
                       fontWeight: FontWeight.w500,
@@ -110,11 +121,11 @@ class ProfileScreenState extends State<ProfileScreen> {
             ),
             child: Column(
               children: [
-                const Text(
-                  'Nguyễn Văn A',
+                Text(
+                  user.username,
                   style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                 ),
-                const SizedBox(height: 4),
+                const SizedBox(height: 8),
                 Container(
                   padding: const EdgeInsets.symmetric(
                     horizontal: 8,
@@ -151,7 +162,7 @@ class ProfileScreenState extends State<ProfileScreen> {
                     ),
                   ],
                 ),
-                const SizedBox(height: 8),
+                const SizedBox(height: 10),
                 _buildDivider(),
               ],
             ),
